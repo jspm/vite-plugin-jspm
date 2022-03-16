e3cf9f620c54c7a8d3a1aa3778687bf3e53969a5
aslemammad <zorofight94@gmail.com>
2022-03-16 14:40:51 +0330

    update

---
+++ test/basic/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
