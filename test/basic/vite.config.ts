e7f634d8446a029c4e79d06e93391fc0ffa1f11e
aslemammad <zorofight94@gmail.com>
2022-03-17 01:38:35 +0330

    let's disable js pollyfills

--- test/basic/vite.config.ts
+++ test/basic/vite.config.ts
@@ -1,9 +1,10 @@
-import type {} from 'vitest'
+import type {} from "vitest";
 import { defineConfig } from "vite";
-import jspmPlugin from 'vite-plugin-jspm'
+import jspmPlugin from "vite-plugin-jspm";
 
 export default defineConfig({
   plugins: [jspmPlugin()],
+  build: { polyfillModulePreload: false, polyfillDynamicImport: false },
   test: {
     threads: false,
   },
