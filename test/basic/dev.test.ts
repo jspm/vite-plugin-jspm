070a8a95bdc3c4ca975385dc361b473920794f28
aslemammad <zorofight94@gmail.com>
2022-03-17 22:29:44 +0330

    add tests for dev

---
+++ test/basic/dev.test.ts
@@ -0,0 +1,37 @@
+import puppeteer from "puppeteer";
+import {
+  createServer,
+  ViteDevServer,
+} from "vite";
+import { describe, afterAll, beforeAll, expect, test } from "vitest";
+
+const url = "http://localhost:3000";
+
+describe("build", async () => {
+  let server: ViteDevServer;
+
+  let browser: puppeteer.Browser;
+  let page: puppeteer.Page;
+
+  beforeAll(async () => {
+    server = await createServer({
+      configFile: "./vite.config.ts",
+      server: { port: 3000 },
+    });
+    server.printUrls();
+
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+  });
+
+  // in dev, page should just render
+  test("basic render", async () => {
+    await page.goto(url);
+    expect(await page.content()).toContain("Hello, world!");
+  });
+
+  afterAll(async () => {
+    await browser.close();
+    server.close();
+  });
+});
