823d041ffa8ead29d175cb811cb96e08dd790bb5
aslemammad <zorofight94@gmail.com>
2022-03-17 22:24:01 +0330

    edit vite.config.ts

--- test/basic/vite.config.ts
+++ test/basic/vite.config.ts
@@ -1,11 +1,7 @@
-import type {} from "vitest";
 import { defineConfig } from "vite";
 import jspmPlugin from "vite-plugin-jspm";
 
 export default defineConfig({
   plugins: [jspmPlugin()],
   build: { polyfillModulePreload: false, polyfillDynamicImport: false },
-  test: {
-    threads: false,
-  },
 });
