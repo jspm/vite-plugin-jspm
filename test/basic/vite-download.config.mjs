8b48c6f9baf135fde0fca2725b942c7f33ec4388
Jaya Krishna <namburu1995@gmail.com>
2023-02-14 20:47:23 +0100

    Adding and fixing tests

--- test/basic/vite-download.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import jspmPlugin from "../../plugin/dist/index.js";
-
-export default defineConfig({
-  plugins: [jspmPlugin({ downloadDeps: true })],
-  build: { polyfillModulePreload: false, polyfillDynamicImport: false },
-});
